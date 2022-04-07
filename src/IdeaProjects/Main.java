4d10f9c57440e6b93bda0eca200e1e8f35abfc2b
babk1nvnuk <babkinartem20@gmail.com>
2022-04-07 22:48:48 +0200

    Initial commit

---
+++ src/IdeaProjects/Main.java
@@ -0,0 +1,26 @@
+package IdeaProjects;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//Задача 6
+ //     int  a = 12, b = 27, c = 44, d = 15, e = 9;
+        // int result = a * (b + (c - d * e));
+        //    System.out.println(result);
+        //  System.out.println(result - 2* result);
+
+
+        //Задача 7
+  //   int a =5;
+    // int b =7;
+    // System.out.println("1 uslovie " + a / a * b);
+      //  System.out.println( "2 uslovie " + b / b* a);
+
+//Задача 8
+
+        int a = 596;
+        int b = (a % 100 /10);
+        System.out.println(b);
+    }
+}
